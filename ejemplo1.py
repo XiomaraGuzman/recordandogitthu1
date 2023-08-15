76f87eb78f332190ccb838a81f25c55c729602c3
309 <309@academicos.cesde.edu.co>
2023-08-15 08:09:13 -0500

    feat:creamos variables primitivas y lista de datos

---
+++ ejemplo1.py
@@ -0,0 +1,14 @@
+#soy un comentario de linea 
+nombreUsuario="Xiomara Guzman Zapata"
+edadUsuario=19
+estaturaUsurios=1.55
+esHinchaDelVerde=True
+
+#salida por consola 
+print(f"su nombre es:{nombreUsuario} y tiene {edadUsuario} años.")
+
+#como crear una lista 
+
+comidasFavoritas=["frisoles", "sushicharron", "mazamorra","carne molida"]
+print(comidasFavoritas)
+print(comidasFavoritas[2])
