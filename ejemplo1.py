b2e1dde87c9c4543debbe8334e394a7df22cf8b7
309 <309@academicos.cesde.edu.co>
2023-08-15 08:39:43 -0500

    feat:se resiben datos por teclado y se castean

--- ejemplo1.py
+++ ejemplo1.py
@@ -1,5 +1,5 @@
 #soy un comentario de linea 
-nombreUsuario="Xiomara Guzman Zapata"
+'''nombreUsuario="Xiomara Guzman Zapata"
 edadUsuario=19
 estaturaUsurios=1.55
 esHinchaDelVerde=True
@@ -12,3 +12,14 @@ print(f"su nombre es:{nombreUsuario} y tiene {edadUsuario} años.")
 comidasFavoritas=["frisoles", "sushicharron", "mazamorra","carne molida"]
 print(comidasFavoritas)
 print(comidasFavoritas[2])
+
+#entradas por tecldo
+
+lugarTrabajoUsuario=input("Digita el lugar de trabajo: ")
+print(f"usted trabaja en :{lugarTrabajoUsuario}")'''
+
+#pedimos dos numeros
+numeroUno=int(input("gigita un numero: "))
+numeroDos=int(input("digite otro numero: "))
+sumatoria=numeroUno+numeroDos
+print(f"la suma de{numeroUno}+{numeroDos}={sumatoria}")
